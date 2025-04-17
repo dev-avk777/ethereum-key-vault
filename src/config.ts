78bd9f75ef148e61ab7052f5fee784e3b76f6774
dev-avk <dev.avk777@gmail.com>
2025-04-17 22:41:43 +0300

    refactor: update config

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,18 @@
 const config = {
   apiUrl:
-    import.meta.env.MODE === 'production'
-      ? import.meta.env.VITE_API_URL || 'https://api.tokenswallet.ru'
-      : import.meta.env.VITE_API_URL ||
-        (window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'http://app:5000'), // Используем app как имя сервиса в docker-compose
+    process.env.MODE === 'production'
+      ? process.env.VITE_API_URL || 'https://api.tokenswallet.ru'
+      : process.env.VITE_API_URL ||
+        (process.env.HOSTNAME === 'localhost' ? 'http://localhost:5000' : 'http://app:5000'), // Используем app как имя сервиса в docker-compose
 
   // Добавляем baseUrl для консистентности
   baseUrl:
-    import.meta.env.MODE === 'production'
-      ? import.meta.env.VITE_FRONTEND_URL || 'https://tokenswallet.ru'
-      : import.meta.env.VITE_FRONTEND_URL || window.location.origin,
+    process.env.MODE === 'production'
+      ? process.env.VITE_FRONTEND_URL || 'https://tokenswallet.ru'
+      : process.env.VITE_FRONTEND_URL || window.location.origin,
 
   // Флаг для определения продакшен-режима
-  isProduction: import.meta.env.MODE === 'production',
+  isProduction: process.env.MODE === 'production',
 
   // Для работы в Docker - автоматически определяем по hostname
   inDocker: window.location.hostname !== 'localhost',
